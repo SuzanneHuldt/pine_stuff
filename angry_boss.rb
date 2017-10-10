9c2e8473f4a39876d6e0ae248e75ce60d9a51663
Suze Huldt <suzemarsupial@gmail.com>
2017-10-10 17:35:03 +0000

    boss program

---
+++ angry_boss.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+puts "Come into the office, what do you have to say for yourself???"
+utterance = gets.chomp
+puts "What? " + utterance.upcase + "??!!! What do you mean " + utterance.upcase + "!??!!"
