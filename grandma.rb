48ac4ba8670dc53f44b9529bbb65635e2a8794f3
Suze Huldt <suzemarsupial@gmail.com>
2017-10-10 17:34:06 +0000

    grandma prgram

---
+++ grandma.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+puts "HI SWEETIE HOW ARE YOU?" 
+while true
+    input = gets.chomp
+    if input == "BYE"
+        puts "BYE HONEY!"
+        break
+    end
+    if input.upcase != input
+    puts "SPEAK UP SONNY"
+        else puts "NO NOT SINCE 1952"
+    end
+end
+     
