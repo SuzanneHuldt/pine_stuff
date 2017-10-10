72b36497fb3f22b1651f47e52625678f8c7e5d36
Suze Huldt <suzemarsupial@gmail.com>
2017-10-10 17:34:38 +0000

    extra grandma changes

---
+++ grandma_extended.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+puts "HI SWEETIE HOW ARE YOU?" 
+while true
+    input = gets.chomp
+    if input == "BYE BYE BYE"
+        puts "BYE HONEY!"
+        break
+    end
+    if input.upcase != input
+    puts "SPEAK UP SONNY"
+        else puts "NO NOT SINCE 1952"
+    end
+end
